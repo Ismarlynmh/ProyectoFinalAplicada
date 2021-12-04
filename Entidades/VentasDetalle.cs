b7d2869acf72d2837391ddf3c69ded499a367b81
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-12-04 12:52:32 -0400

    Entidad VentasDetalle creada

---
+++ Entidades/VentasDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAplicada.Entidades
+{
+    public class VentasDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public int VentaId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+
+        public VentasDetalle()
+        {
+            Id = 0;
+            ProductoId = 0;
+            VentaId = 0;
+            Cantidad = 0;
+            Precio = 0;
+        }
+    }
+}
