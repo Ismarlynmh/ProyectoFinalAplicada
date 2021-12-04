29d6a51f4bd670d297455f48101b1360e3ecd98f
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-12-04 12:57:06 -0400

    Registro de Roles creado

---
+++ UI/Registros/rRoles.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinalAplicada.UI.Registros
+{
+    /// <summary>
+    /// Lógica de interacción para rRoles.xaml
+    /// </summary>
+    public partial class rRoles : Window
+    {
+        public rRoles()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
