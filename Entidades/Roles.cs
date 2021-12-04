3f575ce9c70a9dc93ab46421517c2710d88387b6
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-12-04 01:10:45 -0400

    Creando entidad roles

---
+++ Entidades/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAplicada.Entidades
+{
+    public class Roles
+    {
+
+        [Key]
+        public int RolId { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
