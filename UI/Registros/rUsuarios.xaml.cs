0346e29216a383d8731fca05759e9d707a08d619
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-12-04 12:59:08 -0400

    Registro de Usuarios creado

---
+++ UI/Registros/rUsuarios.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinalAplicada.UI.Registros
+{
+    /// <summary>
+    /// Lógica de interacción para rUsuarios.xaml
+    /// </summary>
+    public partial class rUsuarios : Window
+    {
+        public rUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButtton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RolIdComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
