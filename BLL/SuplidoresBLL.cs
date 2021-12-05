edc4267a2fde020ef9bba184abe1ad7a6b955df0
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-12-05 15:09:03 -0400

    Creando BLL Producto Y Suplidores

---
+++ BLL/SuplidoresBLL.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using ProyectoFinalAplicada.DAL;
+using ProyectoFinalAplicada.Entidades;
+
+
+namespace ProyectoFinalAplicada.BLL
+{
+    public class SuplidoresBLL
+    {
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Suplidores.Any(e => e.SuplidoreId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        /// <summary>
+        /// Permite insertar una entidad en la base de datos
+        /// </summary>
+        /// <param name="suplidore">La entidad que se desea insertar</param>
+
+        public static bool Insertar(Suplidores suplidores)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Suplidores.Add(suplidores);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        /// <summary>
+        /// Permite modificar una entidad en la base de datos
+        /// </summary>
+        /// <param name="suplidore">La entidad que se desea modificar</param>
+        /// 
+
+        public static bool Modificar(Suplidores suplidores)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(suplidores).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var suplidores = contexto.Suplidores.Find(id);
+                if (suplidores != null)
+                {
+                    contexto.Suplidores.Remove(suplidores);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Suplidores Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Suplidores suplidores;
+            try
+            {
+                suplidores = contexto.Suplidores.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return suplidores;
+        }
+
+        public static List<Suplidores> GetList(Expression<Func<Suplidores, bool>> cristerio)
+        {
+            List<Suplidores> lista = new List<Suplidores>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Suplidores.Where(cristerio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+
+            }
+            return lista;
+        }
+
+        public static List<Suplidores> GetSuplidores()
+        {
+            List<Suplidores> lista = new List<Suplidores>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Suplidores.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+
+        }
+    }
+}
