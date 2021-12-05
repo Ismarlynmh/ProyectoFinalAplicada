5ed51803f8956c4f46be1cf019f3546a1ff8833c
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-12-05 11:14:58 -0400

    Creando Entidad Productos

---
+++ Entidades/Productos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada.Entidades
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioDeVenta { get; set; }
+        public decimal PrecioDeCompra { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+
+        [ForeignKey("Suplidores")]
+        public int SuplidorId { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Nombre = string.Empty;
+            Marca = string.Empty;
+            Cantidad = 0;
+            PrecioDeCompra = 0;
+            PrecioDeVenta = 0;
+            FechaIngreso = DateTime.Now;
+            SuplidorId = 0;
+        }
+    }
+}
