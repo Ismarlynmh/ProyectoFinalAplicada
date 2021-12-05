c797cf81d89b55bda12f7e89ede138a7e2e92116
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-12-05 14:22:18 -0400

    BLL de usuarios creada

---
+++ BLL/UsuariosBLL.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAplicada.DAL;
+using ProyectoFinalAplicada.Entidades;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+
+namespace ProyectoFinalAplicada.BLL
+{
+    public class UsuariosBLL
+    {
+        public static bool Guardar(Usuarios usuarios)
+        {
+            if (!Existe(usuarios.UsuarioId))
+            {
+                return Insertar(usuarios);
+            }
+            else
+            {
+                return Modificar(usuarios);
+            }
+        }
+        private static bool Insertar(Usuarios usuarios)
+        {
+            bool ok = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Usuarios.Add(usuarios);
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+        public static bool Modificar(Usuarios usuarios)
+        {
+            bool ok = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(usuarios).State = EntityState.Modified;
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ok;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool ok = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var usuarios = contexto.Usuarios.Find(id);
+
+                if (usuarios != null)
+                {
+                    contexto.Usuarios.Remove(usuarios);
+                    ok = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ok;
+        }
+        public static Usuarios Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Usuarios usuarios;
+
+            try
+            {
+                usuarios = contexto.Usuarios.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return usuarios;
+        }
+
+        public static List<Usuarios> GetList(Expression<Func<Usuarios, bool>> criterio)
+        {
+            List<Usuarios> lista = new List<Usuarios>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Usuarios.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Usuarios.Any(u => u.UsuarioId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+        public static List<Usuarios> GetUsuario()
+        {
+            List<Usuarios> lista = new List<Usuarios>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Usuarios.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
