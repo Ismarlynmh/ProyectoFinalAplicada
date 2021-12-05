b81daa93e1d5fc143d4e8287eff963be33d493bd
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-12-05 14:26:12 -0400

    Contexto de entidades roles, usuarios y ventas

---
+++ DAL/Contexto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using ProyectoFinalAplicada.Entidades;
+
+
+namespace ProyectoFinalAplicada.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet <Ventas> Ventas { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = Data/MendozaCreation.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Roles>().HasData(new Roles
+            {
+                RolId = 1,
+                Descripcion = "Admin"
+            }
+
+            );
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Usuarios>().HasData(new Usuarios
+            {
+                UsuarioId = 1,
+                RolId = 1,
+                Nombres = "Admin",
+                Apellidos = "Admin",
+                Cedula = "88888888888",
+                Sexo = "Femenino",
+                Telefono = "8888888888",
+                Celular = "8888888888",
+                Direccion = "SFM",
+                Email = "admin123@gmail.com",
+                TipoUsuario = "Administrador",
+                FechaIngreso = DateTime.Now,
+                NombreUsuario = "Admin",
+                Contrasena = "Admin"
+            }
+
+
+            );
+
+        }
+    }
+}
