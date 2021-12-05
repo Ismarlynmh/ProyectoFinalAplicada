4e17c24ea8ab29c1e29e281d63963c7c1c52ae03
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-12-05 14:15:14 -0400

    BLL de utilidades creada

---
+++ BLL/Utilidades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada.BLL
+{
+    public class Utilidades
+    {
+        public static int ToInt(string valor)
+        {
+            int retorno = 0;
+
+            int.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+    }
+}
