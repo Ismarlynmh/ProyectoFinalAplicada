95789a0063483dbbc66b322fdb25e0b80fac51c7
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-12-05 11:07:17 -0400

    Creando Entidad ComprasDetalle

---
+++ Entidades/ComprasDetalle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada.Entidades
+{
+    public  class ComprasDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public int CompraId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+
+
+        public ComprasDetalle()
+        {
+            Id = 0;
+            ProductoId = 0;
+            CompraId = 0;
+            Cantidad = 0;
+            Precio = 0;
+
+        }
+    }
+}
