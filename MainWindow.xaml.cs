3421afe068e5ffde14c8f2942128abdafc9da6f2
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-12-06 18:52:04 -0400

    Arreglando codificacion del menu principal

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using ProyectoFinalAplicada.Entidades;
 using ProyectoFinalAplicada.UI.Consultas;
 using ProyectoFinalAplicada.UI.Registros;
 using ProyectoFinalAplicada.BLL;
+using ProyectoFinalAplicada.UI;
 
 namespace ProyectoFinalAplicada
 {
@@ -33,6 +34,10 @@ namespace ProyectoFinalAplicada
             usuario = UsuariosBLL.Buscar(usuarioSiempreActivoId);
             UsuarioActivoTextBox.Text = ("Usuario activo: " + usuario.NombreDeUsuario.ToString() + "\nID Usuario activo: " + usuario.UsuarioId.ToString());
         }
+        public MainWindow()
+        {
+            InitializeComponent();
+         }
         private void UsuarioMenuItem_Click(object sender, RoutedEventArgs e)
         {
 
@@ -48,7 +53,8 @@ namespace ProyectoFinalAplicada
 
         private void ComprasMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            
+            rCompras rCompras = new rCompras();
+            rCompras.Show();
         }
 
         private void SuplidoresMenuItem_Click(object sender, RoutedEventArgs e)
@@ -64,7 +70,8 @@ namespace ProyectoFinalAplicada
 
         private void EmpleadosMenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            rEmpleados rEmpleados = new rEmpleados();
+            rEmpleados.Show();
         }
 
         private void ProductosMenuItem_Click(object sender, RoutedEventArgs e)
@@ -89,21 +96,25 @@ namespace ProyectoFinalAplicada
         private void ConsultarComprasMenuItem_Click(object sender, RoutedEventArgs e)
         {
 
+            cCompras cCompras = new cCompras();
+            cCompras.Show();
         }
 
         private void ConsultarSuplidoresMenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            cSuplidores cSuplidores = new cSuplidores();
+            cSuplidores.Show();
         }
 
         private void ConsultarProductosMenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            cProductos cProductos = new cProductos();
+            cProductos.Show();
         }
 
         private void ConsultarEmpleadosMenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+           
         }
     }
 }
